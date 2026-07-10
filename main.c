a6a28b3b05eb012c6dac6ef602adff8b5c5216f9
jonahmer22 <jonah@jonahmerriam.net>
2026-07-10 01:23:30 -0400

    Added basic shell of the repl

--- main.c
+++ main.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 #include <stddef.h>
 #include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
 
 #include "general.h"
 #include "tokenizer.h"
 #include "deps/arena/arena.h"
+
+#define MAX_REPL_LINE_LENGTH 1024
 
 int main(int argc, char **argv){
     // used for interpreter internals and "global state"
@@ -18,7 +22,21 @@ int main(int argc, char **argv){
 
         goto end;
     }
-    printf("Hello, World!\n");
+
+    // repl
+    fprintf(stdout, "STAK\nA stack based programming language.\nType \'bye\' to exit.\n");
+    fflush(stdout);
+    while(true){
+        fprintf(stdout, "$ ");
+        fflush(stdout);
+
+        char line[MAX_REPL_LINE_LENGTH] = {0};
+        fgets(line, MAX_REPL_LINE_LENGTH, stdin);
+
+        // i think the only way you get "bye\n\0" is by typing only that, the eof is right after anyway, idrc
+        if(memcmp(line, "bye\n\0", 5) == 0)
+            break;
+    }
 
     end:
         arenaDestroy();
